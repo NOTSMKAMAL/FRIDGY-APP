eb6cf7e5fb4b53b6ca3921fb51ce3ab318b3b756
BrianPeluche <brianpeluche10@hotmail.com>
2025-09-14 17:11:23 -0700

    fixed errors

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Reanimated plugin
-      'react-native-reanimated/plugin',
+    presets: [
+      // Disable the old 'react-native-reanimated/plugin'
+      ['babel-preset-expo', { jsxImportSource: 'nativewind', reanimated: false }],
+      'nativewind/babel',
     ],
+    // Optional: you can omit this because the preset will add it when
+    // 'react-native-worklets' is installed (default 'worklets: true')
+    //plugins: ['react-native-worklets/plugin'],
   };
 };
